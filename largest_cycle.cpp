096d6cf0f5b482854c358466c185ba3c141c1f67
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-08-25 17:10:12 -1000

    Update cycle counter to at least function, kinda :relieved:

--- largest_cycle.cpp
+++ largest_cycle.cpp
@@ -2,18 +2,17 @@
 
 using namespace std;
 
-int max(int oldMax, int potentialMax);
-int specialOperation(int seed);
+int findMax(int oldMax, int potentialMax);
+int cycleLength(int seed);
 
 /**
  * Returns an integer.
  * This function asks the user for two integers, then it returns the largest
  * cycle length of all the integers between the two input integers to perform a
- * special operation.
+ * particular operation.
  */
-int main()
-{
-  int rangeBottom, rangeTop, max;
+int main() {
+  int rangeBottom, rangeTop, max = 0;
 
   cout << "Please input the bottom of the range: ";
   cin >> rangeBottom;
@@ -21,14 +20,16 @@ int main()
   cin >> rangeTop;
 
   // iterate over the range, looking for the new max
-  // for (int i = rangeBottom; i <= rangeTop; i++)
-  // {
-  //   // do the special operation.  Store the result.
-  //
-  //   // run the result through max, storing that result.
-  // }
+  for (int i = rangeBottom; i <= rangeTop; i++)
+  {
+    int testLength = cycleLength(i);
 
-  cout << "The longest cycle length was " << max << "\n";
+    if (testLength > max) {
+      max = testLength;
+    }
+  }
+
+  cout << "The longest cycle is this long: " << max << endl;
   return max;
 }
 
@@ -39,25 +40,35 @@ int main()
  * @potentialMax
  * It returns the largest of the two.
  */
-int max(int oldMax, int potentialMax);
-{
-
+int findMax(int oldMax, int potentialMax) {
+  return (potentialMax > oldMax) ? potentialMax : oldMax;
 }
 
 /**
  * Returns an integer.
  * This function takes one param, an integer,
  * @seed
- * It returns the cycle length of the special operation.
+ * It returns the cycle length of the particular operation.
  */
-int specialOperation(int seed)
-{
-  // IF x % 2 != 0
-  //   IF x == 1
-  //     THEN return the cycle length and exit the loop.
-  // ELSE x = x * 3 + 1 and exit the loop.
-  // IF x % 2 == 0
-  //   THEN x /= 2 and go on to the next int.
-  //
+ // Todo: what if it's not a positive int?
+int cycleLength(int seed) {
+  // Off by one error? Is 1 a corner case?
+  //  1 : 1
+  //  2 : 2 1
+  //  8 : 3 10 5 16 8 4 2 1
+  //  3 : 4 2 1
+  //  6 : 5 16 8 4 2 1
+  //  9 : 6 3 10 5 16 8 4 2 1
+  // 17 : 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
+  //  4 : 8 4 2 1
+  // 20 : 9 28 14 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
+  //  7 : 10 5 16 8 4 2 1
 
+  if (seed == 1) {
+    return seed;
+  } else if (seed % 2 == 0) {
+    return 1 + cycleLength(seed / 2);
+  } else {
+    return 1 + cycleLength(seed * 3 + 1);
+  }
 }
