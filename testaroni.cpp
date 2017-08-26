7636119419ae13e2c1e3f2bb0f8c84d02dea8491
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-08-25 17:09:03 -1000

    Add file for cli testing :simple_smile:

---
+++ testaroni.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num, max = 7;
+
+  cout << "\nAs far a James knows, \n\n";
+  cout << "irb  is to ruby as \n";
+  cout << "node is to js as \n";
+  cout << "this is to c++.\n\n";
+
+  cout << "Input something: ";
+  cin >> num;
+
+  cout << "max: " << max << endl;
+  cout << "num: " << num << endl;
+
+  return 0;
+}
