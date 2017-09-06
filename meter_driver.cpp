42a7ff626480959c2b8deb0ae58f7a5f8e23adcc
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-09-06 11:54:02 -1000

    Add constructor, accessor, and mutator functions.

--- meter_driver.cpp
+++ meter_driver.cpp
@@ -1,1 +1,10 @@
 #include "meter.h"
+
+int main()
+{
+  cout << "The driver is running.\n";
+
+  Pedometer meter;
+  cout << "Your default stride length is " <<
+       meter.getStride() << " centimeters.\n";
+}
