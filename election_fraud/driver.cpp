91e40f183541ef8c11278a899a09afc122dfeeb5
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-09-24 18:43:56 -1000

    Add Voter class declaration and member declarations.

---
+++ election_fraud/driver.cpp
@@ -0,0 +1,38 @@
+//  This program simulates an election
+#include <iostream>
+// include file stream?
+// #include "voter.h"
+// #include "voterMembers.cpp"
+using namespace std;
+
+void ballotReader(string ballotFile);
+// reads in the ballot from a separate file, taking the name of the file as a
+// string argument.
+// The ballot file is structured thusly:
+// The first line of the file will contain the number of categories, n.
+// the next n lines will contain the following:
+  // the number of choices for that category,
+  // the name of the category
+  // and the choices for that category. 
+// The end of the ballot information will be marked by a 0.
+
+void voteReader(string votesFile);
+// reads in the posted list of votes from a separate file, taking the name of the
+// file as a string argument.
+
+void tallyVotes(string resultsFile);
+// accounts for all the votes and writes the results of the election
+// to a text file for each ballot item (the string argument),
+// followed by a TAB
+// the number of votes received by each choice for that ballot item,
+// followed by a TAB
+// and the percentage (to 2 decimal places) for each ballot item.
+
+int main()
+{
+  vector<string> votes; // all the votes from the election;
+  ballotReader(/* ballot file name */);
+  voteReader(/* votes file name */);
+  tallyVotes(/* destination file name */);
+  return 0;
+}
---
+++ election_fraud/voter.h
@@ -0,0 +1,41 @@
+// Voter class declaration
+#include <iostream>
+#include "voterMembers.cpp"
+using namespace std;
+
+class Voter
+{
+public:
+  Voter();
+  // Initializes the object with a ballot with no more than 100 questions.
+
+  Voter(int voteCount, string newPositions);
+  // An overloaded constructor that takes the number of votes and
+  // a string of votes (e.g. “ACFHJ”).
+
+  Voter(const Voter& voter_object);
+  // Copy constructor
+
+  ~Voter();
+  // Destructor - returns dynamic memory to the freestore
+
+  Voter& operator=(const Voter &rightVoter);
+  // Overloads assignment operator
+
+  int getId();
+  // returns the voter's id number
+
+  void setId(int newId);
+  // a mutator function that set's the voter's id taking an integer argument.
+
+  char getPositions();
+  // returns the voter's votes as a cstring
+
+  void setPositions(const char newPositions[]);
+  // a mutator function that sets the voter's positions, taking a
+  // character array of votes as an argument.
+
+private:
+  int id; // voter's identification number
+  char *positions; // pointer to dynamic array of this voter's votes
+}
