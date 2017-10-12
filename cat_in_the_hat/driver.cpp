bc69894bdb77204c48f98c08dddb92b28e9fbf62
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-10-11 14:57:18 -1000

    Add fibonacci number calculator.

---
+++ cat_in_the_hat/driver.cpp
@@ -0,0 +1,18 @@
+/*
+This program simulates a game and tells the user how to win.
+
+For input, you will be given a file with a rhyme. For each particular rhyme, you need to determine where Alice needs to stand in line in order to win the game for any given number of friends between 2 and 10. For each input rhyme, save your results to a file.
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // handle situation in which no one wants to play
+
+
+
+  return 0;
+}
