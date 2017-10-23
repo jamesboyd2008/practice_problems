16222e48bba2806312a5db27bb3ea4ba841ea87a
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-10-22 21:05:04 -1000

    Add comments to empty program.

---
+++ lab9exceptions.cpp
@@ -0,0 +1,21 @@
+// This program demonstrates error handling.
+#include <iostream>
+
+using namespace std;
+
+class NotADigit {}; // exception class
+class OutofRange {}; // exception class
+
+/*
+- Get user input, one letter at a time (unbeknownst to them: they'll just
+  be inputting a line of characters (hopefully numbers within range
+  (there will be two error hangling features to ensure this))).
+- throw an exception if they input something that wasn't a number.
+- throw an exception if they input a number out of range.
+
+*/
+int main()
+{
+
+  return 0;
+}
