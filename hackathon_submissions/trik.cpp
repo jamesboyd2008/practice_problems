bfb2734c35f6ec81e1f134796efe65c0cb0c2add
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-10-30 13:48:36 -1000

    Add cup swap program.

--- hackathon_submissions/trik.cpp
+++ hackathon_submissions/trik.cpp
@@ -3,6 +3,10 @@ Jealous of Mirko’s position as head of the village, Borko stormed into his ten
 
 Borko puts three opaque cups onto the table next to each other (opening facing down) and a small ball under the leftmost cup. He then swaps two cups in one of three possible ways a number of times. Mirko has to tell which cup the ball ends up under.
 
+A---> swaps contents of cups 1 & 2
+B---> swaps contents of cups 2 & 3
+C---> swaps contents of cups 1 & 3
+
 Wise Mirko grins with his arms crossed while Borko struggles to move the cups faster and faster. What Borko does not know is that programmers in the back are recording all his moves and will use a simple program to determine where the ball is. Write that program.
 
 Input
@@ -18,16 +22,29 @@ using namespace std;
 
 int main()
 {
-  char letter[50];
-  int three_spots[3], temp;
+  char letter[2];
+  int position = 1;
 
   while (cin.get(letter, 2))
   {
-    // cout << letter << endl;
-    // perform the swaps
-    int temp;
-    
+    if      (letter[0] == 'A')
+    {
+      if      (position == 1) position = 2;
+      else if (position == 2) position = 1;
+    }
+    else if (letter[0] == 'B')
+    {
+      if      (position == 2) position = 3;
+      else if (position == 3) position = 2;
+    }
+    else // (letter[0] == 'C')
+    {
+      if      (position == 1) position = 3;
+      else if (position == 3) position = 1;
+    }
   }
+
+  cout << position << "\n";
 
   return 0;
 }
