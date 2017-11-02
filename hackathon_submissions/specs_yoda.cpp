2ed08089f3a9fddcf8b4be24b8b2439707ddaa69
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-11-01 16:55:19 -1000

    Generate test cases.

--- hackathon_submissions/specs_yoda.cpp
+++ hackathon_submissions/specs_yoda.cpp
@@ -1,7 +1,9 @@
 // test driver for yoda program
 
-#include <iostream>
 #include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -29,7 +31,7 @@ vector<string> yoda(string input1, string input2)
 
   // pad compares on front w/ zeros 'till they're the same size
   auto it1 = compare1.insert(compare1.begin(), 0);
-  while (compare1.size() < compare2.size())
+  while (compare1.size() < compare2.size() + 1)
   {
     it1 = compare1.insert(it1, 0);
   }
@@ -40,7 +42,6 @@ vector<string> yoda(string input1, string input2)
   {
     it2 = compare2.insert(it2, 0);
   }
-
 
   // collide the nums!
   for (int i = 0; i < compare1.size(); i++)
@@ -100,7 +101,36 @@ int main()
     {"2345678", "1234567"},
     {"1", "1"},
     {"1000000000", "1000000000"},
-    {"87004001", "96213890"}
+    {"87004001", "96213890"},
+    {"0", "1"},
+    {"1", "0"},
+    {"1", "2"},
+    {"2", "1"},
+    {"2", "3"},
+    {"3", "2"},
+    {"3", "4"},
+    {"4", "3"},
+    {"4", "5"},
+    {"5", "4"},
+    {"5", "6"},
+    {"6", "5"},
+    {"6", "7"},
+    {"7", "6"},
+    {"7", "8"},
+    {"8", "7"},
+    {"8", "9"},
+    {"9", "8"},
+    {"1", "10"},
+    {"1", "20"},
+    {"1", "30"},
+    {"1", "40"},
+    {"1", "50"},
+    {"1", "60"},
+    {"1", "70"},
+    {"1", "80"},
+    {"1", "90"},
+    {"1", "100"},
+    {"20", "1"}
   };
 
   vector< vector<string> > answers =
@@ -115,7 +145,36 @@ int main()
     {"2345678", "YODA"},
     {"1", "1"},
     {"1000000000", "1000000000"},
-    {"741", "92189"}
+    {"741", "92189"},
+    {"YODA", "1"},
+    {"1", "YODA"},
+    {"YODA", "2"},
+    {"2", "YODA"},
+    {"YODA", "3"},
+    {"3", "YODA"},
+    {"YODA", "4"},
+    {"4", "YODA"},
+    {"YODA", "5"},
+    {"5", "YODA"},
+    {"YODA", "6"},
+    {"6", "YODA"},
+    {"YODA", "7"},
+    {"7", "YODA"},
+    {"YODA", "8"},
+    {"8", "YODA"},
+    {"YODA", "9"},
+    {"9", "YODA"},
+    {"1", "1"},
+    {"1", "2"},
+    {"1", "3"},
+    {"1", "4"},
+    {"1", "5"},
+    {"1", "6"},
+    {"1", "7"},
+    {"1", "8"},
+    {"1", "9"},
+    {"1", "10"},
+    {"2", "1"}
   };
 
   int testNum = 0;
@@ -130,9 +189,38 @@ int main()
     )
       cout << "test " << testNum << ": pass\n";
     else
+    {
       cout << "test " << testNum << ": fail\n";
+      cout << "  m: " << problems[i][0] << "   you said: " << results[0] << endl;
+      cout << "  n: " << problems[i][1] << "  you said: " << results[1] << endl;
+    }
     testNum++;
   }
+
+  testNum = 0;
+
+  // ofstream ofile;
+  // ofile.open("delete_me_yoda.txt");
+  // // test everything  m = 0...99, n = 0...99 (10,000 test cases)
+  // ofile << "m      output line 1\nn      output line 2\n\n";
+  // for (int m = 0; m < 100; m++)
+  // {
+  //   for (int n = 0; n < 100; n++)
+  //   {
+  //     // write test cases to a file
+  //     // write the solutions to those test cases in the same file
+  //     // spot check the answers manually? ==> the bug was id'd, thusly
+  //     results = yoda(to_string(m), to_string(n));
+  //     ofile << left << setw(7) << to_string(m)
+  //           << right << setw(7) << results[0] << endl
+  //           << left << setw(7) << to_string(n)
+  //           << right << setw(7) << results[1] << "\n\n";
+  //
+  //     testNum++;
+  //   }
+  // }
+  // ofile << "Quantity of test cases - 29: " << testNum << endl;
+  // ofile.close();
 
   return 0;
 }
