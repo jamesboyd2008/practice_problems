09d4dee7995da5940b62b3ad4e7dac636fc9ebac
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-11-22 15:38:10 -1000

    Add executive employee class.

---
+++ program_4/Employees/executiveemployee.h
@@ -0,0 +1,16 @@
+// This is the header file executiveemployee.h
+// This is the interface for the class ExecutiveEmployee.
+#ifndef EXECUTIVEEMPLOYEE_H
+#define EXECUTIVEEMPLOYEE_H
+
+#include <string>
+#include "salariedemployee.h"
+
+using namespace std;
+
+class ExecutiveEmployee : public SalariedEmployee
+{
+  
+}
+
+#endif // EXECUTIVEEMPLOYEE_H
