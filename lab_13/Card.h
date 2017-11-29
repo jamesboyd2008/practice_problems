6486f67dd9d7221404f001031561af8c6cde98e3
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-11-28 17:35:29 -1000

    Add comments

--- lab_13/Card.h
+++ lab_13/Card.h
@@ -3,8 +3,12 @@
 class Card
 {
 public:
-  // getters and setters
+  // define default constructor
   Card();
+  // define overloaded constructor
+  // define accessors
+  // define mutators
+  // define overloaded stream insertion operator
 private:
   string suit;
   int value; // 2-10, Jack: 10, Queen: 11, King: 12, Ace: 13
