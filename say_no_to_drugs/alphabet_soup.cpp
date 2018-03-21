3cd0b4714c54230399e247dfe5f6f6fcc54afbc0
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-03-20 21:13:24 -1000

    Scrub input file.

--- say_no_to_drugs/alphabet_soup.cpp
+++ say_no_to_drugs/alphabet_soup.cpp
@@ -1,11 +1,37 @@
-
+# include <algorithm>
+# include <fstream>
 # include <iostream>
+# include <string>
+# include <sstream>
 # include "genBST.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Does this thing run?\n";
+    ifstream kublaKhan;
+    kublaKhan.open("kubla_khan.txt"); // access the input file
+    string line;
+    while (getline(kublaKhan, line)) // read every line
+    {
+        int length = line.size();
+        for (int i = 0; i < length; i++) // remove punctuation from the line
+        {
+            if (ispunct(line[i])) // takes away hyphens, also
+            {
+                line.erase(i--, 1);
+                length = line.size();
+            }
+        }
+        stringstream lineStream(line);
+        string word;
+        while (lineStream >> word) // break the line into words
+        {
+            // make every letter lower case
+            transform(word.begin(), word.end(), word.begin(), ::tolower);
+            cout << word << endl;
+        }
+    }
+
     return 0;
 }
