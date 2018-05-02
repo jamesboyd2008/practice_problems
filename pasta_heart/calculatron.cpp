03c53c73b66c66dabe27c1aa8e88ff7ff90ee513
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-05-01 17:33:17 -1000

    Add faulty djikstra's algo.

--- pasta_heart/calculatron.cpp
+++ pasta_heart/calculatron.cpp
@@ -17,7 +17,7 @@ public:
     int getX();
     int getY();
     int getZ();
-    int distanceTo(Planet destination);
+    int distanceFrom(Planet destination);
 private:
     string name;
     int x, y, z;
@@ -51,7 +51,7 @@ int Planet::getZ()
 {
     return z;
 }
-int Planet::distanceTo(Planet destination)
+int Planet::distanceFrom(Planet destination)
 {
     // d(P_1, P_2) = sqrt( (x_2 - x_1)^2 + (y_2 - y_1)^2 + (z_2 - z_1)^2 )
     double distance = sqrt(
@@ -61,6 +61,9 @@ int Planet::distanceTo(Planet destination)
     );
     return round(distance);
 }
+
+// maybe include an index finder here and convert everything to pointers :)
+// note: arrays pass by reference by default
 
 
 int main()
@@ -96,10 +99,9 @@ int main()
     infile >> cases; // quantity of test cases
     for (int i = 1; i <= cases; i++) // iterate over test cases
     {
-        // vector<Planet> planets;
-        Planet planets[planetCount];
         results += "Case " + to_string(i) + ":\n";
         infile >> planetCount; // receive quantity of planets within test case i
+        Planet planets[planetCount];
         for (int j = 0; j < planetCount; j++) // for each planet...
         {
             infile >> name >> x >> y >> z; // get name and coords
@@ -107,7 +109,6 @@ int main()
             // planets.push_back(planet); // add it to the collection of planets
             planets[j] = planet; // add it to the collection of planets
         }
-
         // create 2D array to hold the distances between planets
         double distances[planetCount][planetCount];
         // populate the distances array with Euclidian distances
@@ -115,40 +116,130 @@ int main()
         {
             for (int k = 0; k < planetCount; k++)
             {
-                distances[j][k] = planets[j].distanceTo(planets[k]);
+                distances[j][k] = planets[j].distanceFrom(planets[k]);
             }
         }
 
-        // print distances[][] for troubleshooting
-        cout << "[\n";
-        for (int j = 0; j < planetCount; j++)
-        {
-            cout << "\t";
-            for (int k = 0; k < planetCount; k++)
-            {
-                cout << distances[j][k] << ", ";
-            }
-            cout << "\n";
-        }
-        cout << "]\n\n";
+        // // print distances[][] for troubleshooting
+        // cout << "[\n";
+        // for (int j = 0; j < planetCount; j++)
+        // {
+        //     cout << "\t";
+        //     for (int k = 0; k < planetCount; k++)
+        //     {
+        //         cout << distances[j][k] << ", ";
+        //     }
+        //     cout << "\n";
+        // }
+        // cout << "]\n\n";
 
 
 
         infile >> portalCount; // receive quantity of worm holes
-        for (int j = 0; j < portalCount; j++)
+        for (int j = 0; j < portalCount; j++) // receive all the portals
         {
-            // make the distance between these two planets a big ol' 0
-            infile >> enterPortal >> exitPortal;
-            // get the indeces of distances[j][k] corresponding to the portal
-                // get index out of planets[];
-
+            infile >> enterPortal >> exitPortal; // get entry and exit planets
+            int entryIndex, exitIndex;
+            for (int k = 0; k < planetCount; k++) // iterate over planets
+            {
+                if (planets[k].getName() == enterPortal) // is this the entry?
+                    entryIndex = k;
+                else if (planets[k].getName() == exitPortal) // is this exit?
+                    exitIndex = k;
+            }
+            distances[entryIndex][exitIndex] = 0; // account for the portal
         }
+
+        // // print distances[][] for troubleshooting
+        // cout << "[\n";
+        // for (int j = 0; j < planetCount; j++)
+        // {
+        //     cout << "\t";
+        //     for (int k = 0; k < planetCount; k++)
+        //     {
+        //         cout << distances[j][k] << ", ";
+        //     }
+        //     cout << "\n";
+        // }
+        // cout << "]\n\n";
+
+
         infile >> queryCount;
         for (int j = 0; j < queryCount; j++)
         {
             infile >> start >> end;
-            results += "The distance from " + start + " to " + end + " is "
-                    +  to_string(roundedDistance) + " parsecs.\n";
+            int startIndex, endIndex;
+            for (int k = 0; k < planetCount; k++) // iterate over planets
+            {
+                if (planets[k].getName() == enterPortal) // is this the entry?
+                    startIndex = k;
+                else if (planets[k].getName() == exitPortal) // is this exit?
+                    endIndex = k;
+            }
+
+            // do the assignment, here
+            int result;
+/*
+djikstra's algo
+*/
+
+        // container for calculated shortest distances, accounting for portals
+            int portalDiscounts[planetCount];
+         // for tracking previously visited planets
+            bool visited[planetCount];
+
+            int tooFar = pow(10, 9); // or infinity
+            // populate container for distances from origin
+            for (int k = 0; k < planetCount; k++)
+            {
+                portalDiscounts[k] = tooFar; // default distance is too far
+                // each planet has yet to be considered for route inclusion
+                visited[k] = false;
+            }
+
+            portalDiscounts[startIndex] = 0; // distance from start to start
+
+            // find the shortest distance from/to every planet
+            for (int k = 0; k < planetCount; k++)
+            {
+                // if a route exists, it's smaller than this
+                int currentShortest = tooFar;
+                // to help track the standing best route
+                int shortestIndex;
+                // iterate over every planet
+                for (int l = 0; l < planetCount; l++)
+                {
+                    if (        visited[l] == false && // not in route, yet
+                        portalDiscounts[l] <= currentShortest) // is shortcut
+                    {
+                        currentShortest = portalDiscounts[l];
+                        shortestIndex = l;
+                    }
+                }
+                visited[shortestIndex] = true; // it's now part of the route
+                // account for new route piece
+                for (int planet = 0; planet < planetCount; planet++)
+                {
+                    if ( !visited[planet] &&
+                         distances[shortestIndex][planet] &&
+                         portalDiscounts[shortestIndex] != tooFar &&
+                            portalDiscounts[shortestIndex] +
+                            distances[shortestIndex][planet] < portalDiscounts[planet]
+                    )
+                    {
+                        portalDiscounts[planet] =
+                            portalDiscounts[shortestIndex] +
+                            distances[shortestIndex][planet];
+                    }
+                }
+            }
+
+
+//*************************** end djikstra's algo ******************************
+            // results += "The distance from " + start + " to " + end + " is "
+            //         +  to_string(roundedDistance) + " parsecs.\n";
+            results += "The distance from " + start + " to " + end + " is " +
+                       to_string(portalDiscounts[endIndex]) + " parsecs.\n";
         }
     }
     infile.close();
