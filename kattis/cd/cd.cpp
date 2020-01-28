8f1ec4a9d3c7ec3fb184ea473dd0d0b03c1c4e38
jamesboyd2008 <jamesboyd2008@gmail.com>
2020-01-27 18:47:20 -1000

    Add documentation.

--- kattis/cd/cd.cpp
+++ kattis/cd/cd.cpp
@@ -2,7 +2,30 @@
 
 From https://open.kattis.com/problems/cd
 
-Title: 
+Title: CD
+Jack and Jill have decided to sell some of their Compact Discs, while they still have some value. They have decided to sell one of each of the CD titles that they both own. How many CDs can Jack and Jill sell?
+
+Neither Jack nor Jill owns more than one copy of each CD.
+
+Input
+The input consists of a sequence of test cases. The first line of each test case contains two positive integers 𝑁 and 𝑀, each at most one million, specifying the number of CDs owned by Jack and by Jill, respectively. This line is followed by 𝑁 lines listing the catalog numbers of the CDs owned by Jack in increasing order, and 𝑀 more lines listing the catalog numbers of the CDs owned by Jill in increasing order. Each catalog number is a positive integer no greater than one billion. The input is terminated by a line containing two zeros. This last line is not a test case and should not be processed.
+
+Output
+For each test case, output a line containing one integer, the number of CDs that Jack and Jill both own.
+
+Sample Input 1:
+3 3
+1
+2
+3
+1
+2
+4
+0 0
+
+Sample Output 1:
+2
+
 */
 #include <iostream>
 
